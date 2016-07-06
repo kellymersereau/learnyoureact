52e51d23abc8b84d80188e7da3aeda006b0a803f
kellymersereau <kelly.mersereau@gmail.com>
2016-07-05 20:02:40 -0400

    updates

--- program.js
+++ program.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var app = express();
-    
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var DOM = React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
+
+var browserify = require('browserify');
+var babelify = require("babelify");
+
 app.set('port', (process.argv[2] || 3000));
 app.set('view engine', 'jsx');
 app.set('views', __dirname + '/views');
 app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
-    
+
 require('babel/register')({
         ignore: false
 });
+
+var TodoBox = require('./views/index.jsx');
 
 var data= [
     {
@@ -21,8 +32,36 @@ var data= [
     }
 ];
 
-app.use('/', function(req, res) {
-    res.render('index', {data: data});
+app.use('/bundle.js', function (req, res) {
+    res.setHeader('content-type', 'application/javascript');
+
+    browserify({ debug: true })
+        .transform(babelify.configure({
+            presets: ["react", "es2015"],
+            compact: false
+        }))
+        .require("./app.js", { entry: true })
+        .bundle()
+        .pipe(res);
 });
-    
+
+app.use('/', function (req, res) {
+    var initialData = JSON.stringify(data);
+    var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+
+    res.setHeader('Content-Type', 'text/html');
+
+    var html = ReactDOMServer.renderToStaticMarkup(body(null,
+        div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+        script({
+            id: 'initial-data',
+            type: 'text/plain',
+            'data-json': initialData
+        }),
+        script({src: '/bundle.js'})
+    ));
+
+    res.end(html);
+});
+
 app.listen(app.get('port'), function() {});
