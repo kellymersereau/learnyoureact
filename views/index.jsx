7f53bf4bab12e35a202e3162fd4ae0edfe91afd9
kellymersereau <kelly.mersereau@gmail.com>
2016-07-06 03:21:17 -0400

    problems

--- views/index.jsx
+++ views/index.jsx
@@ -23,6 +23,7 @@ class TodoList extends React.Component {
         this.changeTitle = this.changeTitle.bind(this);
         this.changeDetail = this.changeDetail.bind(this);
         this.addTodo = this.addTodo.bind(this);
+        this.onDelete = this.onDelete.bind(this);
     }
     changeTitle(e){
         this.setState({
@@ -50,30 +51,27 @@ class TodoList extends React.Component {
             detailValue: ""
         });
     }
+    onDelete(e){
+      let deletedData = this.remove.bind();
+    }
     render() {
         let todo = this.state.data.map(function(obj){
-          // below is my solution to the problem, but this can also be done below in the constructor section -- see the correct answer according to the tutorial below
-          // if(todo.state.checked == true){
-          //     return <Todo title={obj.title} key={obj.title} style={style.checkedTodo}>{obj.detail}</Todo>
-          // } else{
-          //     return <Todo title={obj.title} key={obj.title} style={style.notCheckedTodo}>{obj.detail}</Todo>
-          // }
-            return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>
+            return <Todo onDelete={obj.deletedData} title={obj.title} key={obj.title}>{obj.detail}</Todo>
         });
         return (
             <div className="todoList">
                 <div>
-                    Title: <input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
-                    Detail: <input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
-                    <button onClick={{border: "2px solid black"}}>
+                    <span>Title:</span><input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
+                    <span>Detail:</span><input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
+                    <button onClick={this.addTodo}>Add</button>
                 </div>
-                <table style={style.tableContent}>
+                <table style={{border: "2px solid black"}}>
                     <tbody>
                         {todo}
                     </tbody>
                 </table>
             </div>
-        );
+        )
     }
 }
 
@@ -82,18 +80,13 @@ class Todo extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            checked: false
-            // this is the tutorials solution -- put the line below under constructor and then change the handleChange below
-            TodoStyle = style.notCheckedTodo
+            checked: false,
+            TodoStyle: style.notCheckedTodo
         };
         this.handleChange = this.handleChange.bind(this);
+        this._onDelete = this._onDelete.bind(this);
     }
-    //this is how the handleChange used to look before the tutorials solution
-    // handleChange(){
-    //     this.setState({checked: !this.state.checked});
-    // }
 
-    //this is the new handleChange function according to the tutorial solution
     handleChange(e){
         this.setState({
           checked: e.target.checked
@@ -108,16 +101,25 @@ class Todo extends React.Component {
           });
         }
     }
+    _onDelete() {
+      this.props.onDelete(this.props.title)
+    };
 
     render(){
         return (
-          //the way i did it just has this as <tr> -- the way it looks below is how it should look according to the tutorial
             <tr style={this.state.TodoStyle}>
-                <td style={style.tableRow}>
+                <td style={style.tableContent}>
+                    <button onClick={this._onDelete}>X</button>
+                </td>
+                <td style={style.tableContent}>
                     <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
                 </td>
-                <td style={style.tableRow}>{this.props.title}</td>
-                <td style={style.tableRow}>{this.props.children}</td>
+                <td style={style.tableContent}>
+                    {this.props.title}
+                </td>
+                <td style={style.tableContent}>
+                    {this.props.children}
+                </td>
             </tr>
         );
     }
